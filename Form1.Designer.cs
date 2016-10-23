59d228777c8130947848298f98d1b6641f47a3a6
Mustafa ER <mail@mustafaer.pro>
2016-10-23 04:12:25 +0300

    text_to_mors_converter_with_arduino
    First upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace text_to_mors_converter_with_arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rtxt_input = new System.Windows.Forms.RichTextBox();
+            this.rtxt_output = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lst_serial_window = new System.Windows.Forms.ListBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rtxt_output);
+            this.groupBox1.Controls.Add(this.rtxt_input);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(809, 217);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TEXT CONVERT";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_send);
+            this.groupBox2.Controls.Add(this.btn_clear);
+            this.groupBox2.Controls.Add(this.btn_convert);
+            this.groupBox2.Location = new System.Drawing.Point(0, 223);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(501, 241);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "OPERATION";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.lst_serial_window);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(507, 223);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(302, 241);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "SERİAL";
+            // 
+            // rtxt_input
+            // 
+            this.rtxt_input.Location = new System.Drawing.Point(0, 44);
+            this.rtxt_input.Name = "rtxt_input";
+            this.rtxt_input.Size = new System.Drawing.Size(323, 167);
+            this.rtxt_input.TabIndex = 0;
+            this.rtxt_input.Text = "";
+            // 
+            // rtxt_output
+            // 
+            this.rtxt_output.Location = new System.Drawing.Point(329, 44);
+            this.rtxt_output.Name = "rtxt_output";
+            this.rtxt_output.ReadOnly = true;
+            this.rtxt_output.Size = new System.Drawing.Size(480, 167);
+            this.rtxt_output.TabIndex = 1;
+            this.rtxt_output.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input Text :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Text :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "COM : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Serial Window :";
+            // 
+            // lst_serial_window
+            // 
+            this.lst_serial_window.FormattingEnabled = true;
+            this.lst_serial_window.ItemHeight = 16;
+            this.lst_serial_window.Location = new System.Drawing.Point(9, 78);
+            this.lst_serial_window.Name = "lst_serial_window";
+            this.lst_serial_window.Size = new System.Drawing.Size(287, 148);
+            this.lst_serial_window.TabIndex = 2;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(6, 29);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(489, 32);
+            this.btn_convert.TabIndex = 0;
+            this.btn_convert.Text = "CONVERT";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(6, 67);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(489, 32);
+            this.btn_clear.TabIndex = 1;
+            this.btn_clear.Text = "CLEAR";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(6, 105);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(489, 32);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "SEND";
+            this.btn_send.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 465);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtxt_output;
+        private System.Windows.Forms.RichTextBox rtxt_input;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox lst_serial_window;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace text_to_mors_converter_with_arduino
+{
+    public partial class Form1 : Form
+    {
+        string[] alfabe = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "Y", "Z", "W", "X", "Q", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ".", "?", " " };
+        string[] mors = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "..", "---", ".--.", ".-.", "...", "-", "..-", "...-", "-.--", "--..", ".--", "-..-", "--.-", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "--..--", ".-.-.-", "..--..", " " };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string gelenmetin = rtxt_input.Text.ToUpper();
+
+                if (gelenmetin == "")
+                {
+                    //doldur gel
+                }
+                else
+                {
+                    ///metin var
+                    for (int i = 0; i < gelenmetin.Length; i++)
+                    {
+                        string gelenharf = gelenmetin[i].ToString();
+                        int sira = Array.IndexOf(alfabe, gelenharf.ToString());
+                        if (sira != -1)
+                        {
+                            rtxt_output.Text = rtxt_output.Text + mors[sira];
+                        }
+                        else
+                        {
+                            MessageBox.Show("hatalı karakter");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu ! " + ex.ToString());
+            }
+        }
+    }
+}
